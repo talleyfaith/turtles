1749d99679d1f63b4a8c66a10d65148f526c3e48
Jon Cooper <joncoop@users.noreply.github.com>
2017-12-18 13:15:28 -0500

    Add files via upload

--- star2.py
+++ star2.py
@@ -1,16 +1,18 @@
 from turtle import *
 
-color('red', 'yellow')
+line = 'red'
+fill = 'yellow'
+points = 12
+size = 100
 
+angle = 180 - (360 / points)
+color(line, fill)
 begin_fill()
 
-while True:
-    forward(200)
-    left(170)
-    
-    if abs(pos()) < 1:
-        break
+for i in range(points):
+    forward(size)
+    left(angle)
     
 end_fill()
-
+    
 done()
